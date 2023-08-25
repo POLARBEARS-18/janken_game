8d8082311e36f0d45ac178859e12497510cbe18b
POLARBEARS-18 <r.magic.rx.8@gmail.com>
2023-08-26 00:09:40 +0900

    要らない処理削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
-import type { InlineConfig } from 'vitest'
-import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
-import path, { join } from 'path'
+import path from 'path'
 import { readdirSync } from 'fs'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-// vitestを使用できるようにinterfaceを拡張
-interface VitestConfigExport extends UserConfig {
-  test: InlineConfig
-}
 
 // インポートする際のエイリアスを動的に作成する。
 const absolutePathAliases: { [key: string]: string } = {}
@@ -28,41 +21,38 @@ srcRootContent.forEach((directory) => {
 })
 
 // https://vitejs.dev/config/
-export default defineConfig(
-  {
-    plugins: [
-      react({
-        jsxImportSource: '@emotion/react',
-      }),
-      checker({
-        typescript: true,
-        eslint: {
-          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-          dev: {
-            logLevel: ['error'],
-          },
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        dev: {
+          logLevel: ['error'],
         },
-      }),
-      tsconfigPaths(), //vite-tsconfig-paths
-    ],
-    esbuild: {
-      logOverride: { 'this-is-undefined-in-esm': 'silent' },
-    },
-    resolve: {
-      alias: {
-        // ...absolutePathAliases,
-        '~/': `${__dirname}/src/`,
       },
+    }),
+    tsconfigPaths(), //vite-tsconfig-paths
+  ],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+  resolve: {
+    alias: {
+      // ...absolutePathAliases,
+      '~/': `${__dirname}/src/`,
     },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: [`./src/setupTests.ts`],
-      testTimeout: 8000,
-      alias: {
-        '~': `/src`,
-      },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: [`./src/setupTests.ts`],
+    testTimeout: 8000,
+    alias: {
+      '~': `/src`,
     },
-  }
-  // as VitestConfigExport
-)
+  },
+})
